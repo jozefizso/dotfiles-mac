2928dc4acba4f2b0a3097340f7b31bc2748d3668
Jozef Izso <jozef.izso@gmail.com>
2024-03-27 14:28:45 +0100

    Use `nvm` installation script v0.39.7

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,7 +21,7 @@ brew update
 if ! command -v nvm >/dev/null; then
   echo "Installing nvm"
 
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
