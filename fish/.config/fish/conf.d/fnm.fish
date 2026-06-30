07cb47b371284f6f6c3a85465016dcd863e84355
Jozef Izso <jozef.izso@gmail.com>
2026-06-30 12:48:28 +0200

    Add `fnm_upgrade` function

--- fish/.config/fish/conf.d/fnm.fish
+++ fish/.config/fish/conf.d/fnm.fish
@@ -1,4 +1,10 @@
 
 if type -q fnm
   fnm env --use-on-cd --shell fish | source
+
+  function fnm_upgrade
+    fnm exec --using=$argv[1] npm ls --global --json \
+      | jq -r '.dependencies | to_entries[] | .key+"@"+.value.version' \
+      | xargs npm i -g
+  end
 end
